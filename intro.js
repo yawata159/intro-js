723d8284cdcfbe81ffecda885209911a51b69189
AWinnerIsYou <seijiyawata@gmail.com>
2016-12-16 14:40:19 -0500

    done

--- intro.js
+++ intro.js
@@ -1,22 +1,41 @@
 var title = document.getElementsByTagName("h1")[0];
 var ol = document.getElementById("thelist");
-var button = document.getElementById("b");
-    
+var ol2 = document.getElementById("fibonacci");
+var li = document.getElementsByTagName("li");
+var button1 = document.getElementById("b");
+var button2 = document.getElementById("b2");
+
 // Add handlers to existing list elements
-for (var i = 0; i < ol.children.length; i++) {
-    var elem = ol.children[i];
+for (var i = 0; i < li.length; i++) {
+    var elem = li[i];
     addEvents(elem);
 }
 
 // Button click handler
-button.addEventListener("click", addListElement)
+button1.addEventListener("click", addListElement)
+button2.addEventListener("click", addFib);
 
 // When the button is clicked, append a li element with handlers attached
 function addListElement(e) {
     var newElem = document.createElement("li");
-    newElem.innerHTML = "BRAND NEW!!!"
-    addEvents(newElem)
+    newElem.innerHTML = "BRAND NEW!!!";
+    addEvents(newElem);
     ol.appendChild(newElem);
+}
+
+function addFib(e) {
+    var newElem = document.createElement("li");
+    var index = ol2.children.length;
+    newElem.innerHTML = fib(index);
+    addEvents(newElem);
+    ol2.appendChild(newElem);
+}
+
+function fib(index) {
+    if (index == 0 || index == 1) {
+        return 1;
+    }
+    return fib(index-1) + fib(index-2);
 }
 
 function addEvents(li) {
